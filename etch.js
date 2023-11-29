eb727ffcbf4944e68a4fa34f5bc482b0ac580eaf
Sandboks <sandboxasdf@gmail.com>
2023-11-29 12:11:20 +1100

    button to resize added

--- etch.js
+++ etch.js
@@ -1,5 +1,21 @@
+let button = document.querySelector("#RESIZE");
+
+button.addEventListener('click', () => {
+    let newSize = prompt("Enter new grid size [0-128]");
+    newSize = parseInt(newSize);
+    if (isNaN(newSize) || newSize > 128 || newSize < 0) {
+        alert("Invalid input. Please enter an integer number [0-128]");
+    }
+    else {
+        createGrid(newSize);
+    }
+    
+});
+
 function createGrid(numSquares) {
     const container = document.querySelector('.gridContainer');
+
+    destroyGrid();
     
     for (let h = 0; h < numSquares; h++) {
         let newRow = document.createElement("div");
@@ -31,4 +47,19 @@ function createGrid(numSquares) {
     }
 }
 
-createGrid(16);
+function destroyGrid() {
+    let squares = document.querySelectorAll('.gridSquare');
+
+    squares.forEach((square) => {
+        // to remove the event listeners, first replace the square with a clone of itself
+        square.replaceWith(square.cloneNode(true));     
+        square.remove();
+    })
+
+    let rows = document.querySelectorAll(".gridRow");
+    rows.forEach((row) => {
+        row.remove();
+    })
+}
+
+createGrid(4);
